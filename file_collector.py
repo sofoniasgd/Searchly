af2d60777692df8d5809ad1caf5c41c978c6f744
sofoniasgd <zephaniahsofa@gmail.com>
2024-12-02 15:20:17 +0300

    feat(database): updated file collection script

--- file_collector.py
+++ file_collector.py
@@ -1,40 +1,47 @@
 import os
-from app import Document
-
 from app.app import app, db
-from your_flask_app.models import YourModel  # Import your models
+from app.app import Documents  # Import your models
 
 # Create an app context for the database
 app = create_app()
-with app.app_context():
-    # Perform database operations
-    new_record = YourModel(column1="value", column2="value")
-    db.session.add(new_record)
-    db.session.commit()
+
+parent_dir = '/mnt/c/Users/metasebia/Documents/projects/Searchly/test_directory'
+woreda_list = os.scandir(parent_dir)
+# dict to store the woreda number and the pdfs
+master_dict = {}
+file_list = []
+for woreda in woreda_list:
+    woreda_name = woreda.name
+    root_path = woreda.path
+    # use os.walk to get all the files in the directory
+    for root, dirs, files in os.walk(woreda.path):
+        file_list = files
+    # add paths to the files
+    for doc in file_list:
+        file_path = os.path.join(root_path, doc)
+        if woreda_name in master_dict:
+            master_dict[woreda_name].append(file_path)
+        else:
+            master_dict[woreda_name] = [file_path]
+
+#for woreda in master_dict:
+#    print(woreda)
+#    print(master_dict[woreda])
+#``    print('-----------------------')
+
+# store the data for each woreda
+for woreda in master_dict:
+    print(f"Adding {woreda} to the database")
+    for file_path in master_dict[woreda]:
+        file_name = os.path.basename(file_path)
+        # create a new document object
+        new_document = Documents(tin_number=file_path[:10], name=file_name, location=woreda, parent_dir=parent_dir,
+                                 file_path=file_path)
+        db.session.add(new_document)
+        db.session.commit()
+        print(f"Added {file_name} to the database")
 
 
-def populate_db(directory):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.pdf'):
-                tin_number = file[:10]  # Assuming the first 10 characters of the filename are the TIN
-                file_path = os.path.join(root, file)
-                parent_dir = os.path.basename(root)
 
-                # Add the data to the database
-                new_doc = Document(
-                    tin_number=tin_number,
-                    name="Unknown",  # You can modify to fetch name if available
-                    location="Unknown",  # Modify to include location if applicable
-                    parent_dir=parent_dir,
-                    file_path=file_path
-                )
-                db.session.add(new_doc)
 
-    db.session.commit()
-    print("Database populated!")
 
-if __name__ == '__main__':
-    # Set the directory where PDFs are stored
-    directory = '/path/to/your/pdf/directory'
-    populate_db(directory)
